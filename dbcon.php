3a4494dbf12e0258430f489d9c127508c6b610b6
Yash Pandit <yashpandit1995@gmail.com>
2017-11-11 11:42:37 +0530

    changes in fonts

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,18 @@
 <?php
-$conn = new mysqli("localhost", "root", "","fatahcareer");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+$localhost = '127.0.0.1';
+
+if ($_SERVER['REMOTE_ADDR'] == $localhost){
+    $conn = new mysqli("localhost", "root", "", "fatahcareer");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 }
+else {
+    $conn = new mysqli("career.fatah.co", "root", "careerfatah", "fatahcareer");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+}
+
 ?>
